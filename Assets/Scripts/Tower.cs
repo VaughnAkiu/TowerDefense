1887dc05f930de6fcb81993cd1787e6fcb305ef9
Vaughn Akiu <vkakiu@gmail.com>
2021-01-01 17:23:40 -0500

    adding targeting and shooting
    -basic closest range targetting
    -added the start of shooting, no projectiles yet

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -8,18 +8,69 @@ public class Tower : MonoBehaviour
     public Transform target;
     public float range = 1f;
 
+    public string enemyTag = "Enemy";
+
+
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
+
+    //check every definied amount of time for targets
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+
+
     }
 
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+
+
+    //turret range indicator
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
