6fa3d0b50d2bcd4ae903c351c7f9b73f06d22862
Vaughn Akiu <vkakiu@gmail.com>
2021-01-06 15:00:50 -0500

    updating tooltip script , getting the basis going

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tooltip : MonoBehaviour
 {
 
+    private Text tooltipText;
+    private RectTransform backgroundRectTransform;
 
-    private void ShowToolTip()
+
+    private void Awake()
     {
+        backgroundRectTransform = transform.Find("background").GetComponent<RectTransform>();
+        tooltipText = transform.Find("text").GetComponent<Text>();
 
+
+        ShowToolTip("Random tooltip text");
+    }
+
+    private void ShowToolTip(string tooltipString)
+    {
+        gameObject.SetActive(true);
+
+        tooltipText.text = tooltipString;
+        float textPaddingSize = 4f;
+        Vector2 backgroundSize = new Vector2(tooltipText.preferredWidth + textPaddingSize * 2f, tooltipText.preferredHeight + textPaddingSize * 2f);
+        backgroundRectTransform.sizeDelta = backgroundSize;
     }
 
     private void HideToolTip()
     {
-
+        gameObject.SetActive(false);
     }
 
 }
