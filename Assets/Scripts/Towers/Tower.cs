bb83c18c943ab80d985c015281904fcafde0a3ed
Vaughn Akiu <vkakiu@gmail.com>
2021-02-23 10:40:47 -0500

    changing tower shoot sound to be louder

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -86,7 +86,7 @@ public class Tower : MonoBehaviour
         GameObject projectileGameObject = (GameObject)Instantiate(projectilePreFab, firePoint.position, firePoint.rotation);
         Projectile projectile = projectileGameObject.GetComponent<Projectile>();
         //audioSource.PlayOneShot(shootNoise, 0.5f);
-        AudioSource.PlayClipAtPoint(shootNoise, transform.position, 0.1F);
+        AudioSource.PlayClipAtPoint(shootNoise, transform.position, 0.5F);
 
         if (projectile != null)
         {
